1ff627d0e113aba523c1ac598ffc1cf88e9cc371
irenkorneyeva <korneyeva.irina@gmail.com>
2023-07-18 22:34:00 +0700

    Lesson#1 test#2

--- JavaAppiumAutomation/JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/JavaAppiumAutomation/src/MainClassTest.java
@@ -4,11 +4,9 @@ import org.junit.Test;
 public class MainClassTest extends MainClass
 {
     @Test
-    public void testGetLocalNumber(){
-        int expected = 14;
-        int returned = getLocalNumber();
-
-        Assert.assertTrue("Returned value is not 14 !",expected == returned);
+    public void testGetClassNumber(){
+        int returned = getClassNumber();
+        Assert.assertTrue("Returned value is less than 45 !",returned > 45);
     }
 
 }
