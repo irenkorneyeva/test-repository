3004267076cec3ddfec60335d25f28bea6782296
irenkorneyeva <korneyeva.irina@gmail.com>
2023-07-18 23:07:06 +0700

    Lesson#1 test#3

--- JavaAppiumAutomation/JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/JavaAppiumAutomation/src/MainClassTest.java
@@ -1,12 +1,21 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.sql.SQLOutput;
+
 public class MainClassTest extends MainClass
 {
     @Test
-    public void testGetClassNumber(){
-        int returned = getClassNumber();
-        Assert.assertTrue("Returned value is less than 45 !",returned > 45);
+    public void testGetClassString(){
+        String returned = getClassString();
+        boolean match1 = returned.contains("Hello");
+        boolean match2 = returned.contains("hello");
+
+        if (!match1 && !match2) {
+            throw new AssertionError("There is no such substring in the string");
+        } else {
+            System.out.println("There is such substring in the string");
+        }
     }
 
 }
